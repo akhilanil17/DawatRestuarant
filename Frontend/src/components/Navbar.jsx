6f47d6c90792e20451025249548ec08bccdcb9ae
akhilanil17 <akhilanil17@gmail.com>
2022-03-28 12:49:35 +0530

    changed it to responsive

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,57 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import MainLogo from "../images/main_logo.png";
 import MainLogo_black from "../images/main_logo_black.png";
+import MenuIcon from "../images/menu-64.png";
+import MenuIcon_black from "../images/menu-64-black.png";
 
 const Navbar = (props) => {
+
+    const [buttonClick,setButtonClick] = useState(false)
+    const toggleMenu = () => {
+        if(buttonClick===true){
+            setButtonClick(false)
+        }else {
+            setButtonClick(true)
+        }
+    }
+
     return (
         <div className={props.scrollChange ? "navBar navBar_scroll":"navBar"}>
-            <div className="logo">
-                <img src={props.scrollChange ? MainLogo_black: MainLogo} />
-            </div>
-            <div className="nav-items subHeading2">
-                <ul>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"} 
-                            id={props.value}
-                            href="#">HOME</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"}
-                            id={props.value}
-                            href="#ourStory">ABOUT US</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"} 
-                            id={props.value}
-                            href="#specialPackage">OFFER</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"} 
-                            id={props.value}
-                            href="#">MENU</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"} 
-                            id={props.value}
-                            href="#">TEAM</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"} 
-                            id={props.value}
-                            href="#">BLOG</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"}
-                            id={props.value}
-                            href="#Feedback">FEEDBACK</a></li>
-                    <li>
-                        <a  
-                            className={props.scrollChange ? "navBarItems_scroll":"navBarItems"}
-                            id={props.value}
-                            href="#Contact">CONTACT</a></li>
+            <img className="logo" src={props.scrollChange ? MainLogo_black: MainLogo} />
+            <nav className="nav-items subHeading2">
+                <ul className={buttonClick ? "MenuList_click" :"MenuList"}>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#">HOME</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#ourStory">ABOUT US</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#specialPackage">OFFER</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#">MENU</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#">TEAM</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#">BLOG</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#Feedback">FEEDBACK</a></li>
+                    <li><a className={props.scrollChange ? "navBarItems_scroll":""} href="#Contact">CONTACT</a></li>
                 </ul>
-            </div>
+            </nav>
+            <img className={buttonClick ? "menuIcon_rotate" :"menuIcon"} src={props.scrollChange ? MenuIcon_black : MenuIcon} onClick={toggleMenu} />
         </div>
     )
 }
--- Frontend/src/components/OurStory.jsx
+++ Frontend/src/components/OurStory.jsx
@@ -5,13 +5,16 @@ import Signature from "../images/about_sign.png";
 const OurStory = () => {
     return (
         <div className="ourStory" id="ourStory">
-            <img src={OurStoryBackground} />
+            <img className="ourStoryBg" src={OurStoryBackground} />
             <h1 className="mainTitle">Our Story</h1>
             <div className="story">
                 <p className="leftbox"><strong>L</strong>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 <p className="rightbox">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-                <img src={Signature} />
             </div>
+            <div className="signature">
+                <img className="storySignature" src={Signature} />
+            </div>
+            
         </div>
     )
 }
