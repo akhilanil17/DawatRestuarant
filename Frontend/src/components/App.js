e599fd4b08e105539b99144f43ba394ef33e3cfa
akhilanil17 <akhilanil17@gmail.com>
2022-04-18 18:31:16 +0530

    updated gh-pages

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import DashBoard from "./DashBoard";
 import Topbar from "./Topbar";
 import Success from "./Success";
